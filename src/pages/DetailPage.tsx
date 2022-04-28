1523624b3b3af6bd09b2e88e1a4fbc62c7b84849
fredkeemhaus <fredkeemhaus@gmail.com>
2022-04-28 23:50:47 +0900

    Component design - Detail(1)

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import Tabs from "../components/Tabs";
 
 type Params = {
   id: string;
 };
 
+type Tab = "about" | "stats" | "evolution";
+
 const DetailPage: React.FC = () => {
-  const { id } = useParams();
-  return <div>Detail Page id: {id}</div>;
+  const { id } = useParams<Params>();
+  const [selectedTab, setSelectedTab] = useState<Tab>("about");
+
+  const handleClick = (tab: Tab) => {
+    setSelectedTab(tab);
+  };
+
+  return (
+    <div>
+      <Tabs
+        tab={selectedTab}
+        onClick={handleClick}
+        color={{ name: "red", url: "" }}
+      />
+    </div>
+  );
 };
 
 export default DetailPage;
